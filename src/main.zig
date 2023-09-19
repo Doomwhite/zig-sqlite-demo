5c25a804e025c1e96d618ab5ab09054bd8d96964
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-09-18 22:37:48 -0300

    Testing stuff

--- src/main.zig
+++ src/main.zig
@@ -12,11 +12,11 @@ pub fn main() anyerror!void {
 
     var db = try sqlite.Db.init(.{
         .mode = sqlite.Db.Mode{ .Memory = {} },
-        .open_flags = .{ .write = true },
+        .open_flags = .{ .write = true, .create = true },
     });
     defer db.deinit();
 
-    try db.exec("CREATE TABLE user(id integer primary key, age integer, name text)", .{}, .{});
+    try db.exec("CREATE TABLE IF NOT EXISTS user(id integer primary key, age integer, name text)", .{}, .{});
 
     const user_name: []const u8 = "Vincent";
 
@@ -34,7 +34,7 @@ pub fn main() anyerror!void {
     const user_opt = try db.oneAlloc(User, allocator, "SELECT id, age, name FROM user WHERE name = $name{[]const u8}", .{}, .{
         .name = user_name,
     });
-    try std.testing.expect(user_opt != null);
+    // try std.testing.expect(user_opt != null);
     if (user_opt) |user| {
         std.debug.print("select user: id={d} age={d} name={s}\n", .{ user.id, user.age, user.name });
 
@@ -46,14 +46,26 @@ pub fn main() anyerror!void {
     }
 
     // Read single integers; reuse the same prepared statement
-    var stmt = try db.prepare("SELECT id FROM user WHERE age = $age{u32}");
+    // var stmt = try db.prepare("SELECT id FROM user WHERE age = $age{u32}");
+    // defer stmt.deinit();
+    //
+    // const id1 = try stmt.one(usize, .{}, .{@as(u32, 34)});
+    // if (id1) |value| {
+    //     try std.testing.expectEqual(@as(usize, 10), value);
+    // }
+    //
+    // stmt.reset();
+    //
+    // const id2 = try stmt.one(usize, .{}, .{@as(u32, 84)});
+    // if (id2) |value| {
+    //     try std.testing.expectEqual(@as(usize, 20), value);
+    // }
+
+    var stmt = try db.prepare("SELECT id, name, age FROM user");
     defer stmt.deinit();
 
-    const id1 = try stmt.one(usize, .{}, .{@as(u32, 34)});
-    try std.testing.expectEqual(@as(usize, 10), id1.?);
-
-    stmt.reset();
-
-    const id2 = try stmt.one(usize, .{}, .{@as(u32, 84)});
-    try std.testing.expectEqual(@as(usize, 20), id2.?);
+    var iter = try stmt.iterator(User, .{});
+    while (try iter.nextAlloc(allocator, .{})) |row| {
+        std.log.info("row: {any}", .{row});
+    }
 }
